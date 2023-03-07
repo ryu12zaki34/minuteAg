896c0dee7171f6615f40db2a5d08fbd4e2b57383
ryuzaki <anxorguani@gmail.com>
2023-03-07 21:52:07 +0300

    added

--- app/src/main/java/com/example/myminute/MainActivity.kt
+++ app/src/main/java/com/example/myminute/MainActivity.kt
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var minute: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val dateButton: Button = findViewById(R.id.start1)
         val date: TextView =findViewById(R.id.text1)
-        val minute: TextView = findViewById(R.id.text2)
-    }
+        minute=findViewById(R.id.text2)
+        dateButton.setOnClickListener {
+            openCalendar()
 
+        }
+    }
+    fun openCalendar ()
+    {
+        minute?.text="11.11.2002"
+        Toast.makeText(this, "this button works slay", Toast.LENGTH_SHORT).show()
+    }
 }
